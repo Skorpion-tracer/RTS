1aeb38eb7bee165ddab8d4c4b61192958c35343f
Skorpion-tracer <Skorpion-tracer@yandex.ru>
2022-02-27 20:13:44 +0300

    Update

---
+++ RTS/Assets/Scripts/Core/CoreInstaller.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class CoreInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+    }
+}
+
